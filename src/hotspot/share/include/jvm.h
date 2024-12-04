687218951a0605506aeca6f509931716223da915
Vladimir Ivanov <vladimir.x.ivanov@oracle.com>
2024-12-04 15:14:53 -0800

    update

--- src/hotspot/share/include/jvm.h
+++ src/hotspot/share/include/jvm.h
@@ -1137,6 +1137,11 @@ JVM_PrintWarningAtDynamicAgentLoad(void);
 /*
  * FIXME
  */
+JNIEXPORT jlong JNICALL
+JVM_GetCPUFeatures(JNIEnv* env);
+/*
+ * FIXME
+ */
 JNIEXPORT jstring JNICALL
 JVM_GetCPUFeaturesString(JNIEnv* env);
 
